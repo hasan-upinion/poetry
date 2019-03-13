a961230d97eab50e7c9397b7ce6811bbd3428174
hasan-upinion <hasan-sh@upinion.com>
2019-03-13 14:33:13 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import './App.css';
+
+const App = () => {
+  const [counter, setCounter]= useState(0);
+  return (
+    <div className="App">
+      {counter}
+      <button onClick={() => setCounter(counter + 1)}>increment</button>
+    </div>
+  )
+}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.tsx</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
